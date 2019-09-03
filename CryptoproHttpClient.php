e5cab2ab5b5f8afd70d6baf3c83c770f73a24b61
Nikita Zarubin <nikita-dimitrov@mail.ru>
2019-09-03 16:14:56 +0300

    Update CryptoproHttpClient.php

--- CryptoproHttpClient.php
+++ CryptoproHttpClient.php
@@ -1,3 +1,75 @@
 <?php
+namespace nikserg\cryptoprohttpclient;
+
 class CryptoproHttpClient {
+
+    /**
+     * @var string Путь к исполняемому файлу Curl КриптоПро
+     */
+    public static $curlExec = '/opt/cprocsp/bin/amd64/curl';
+
+
+    public static function request($url, $requestBody = null, $certificateThumbprint = null, $headers = [])
+    {
+        $requestBody = str_replace("'", "'\"'\"'", $requestBody);
+
+        $shellCommand = self::$curlExec .
+            ' -k -s '. $url . ' '.
+            '--header "Content-Type: text/xml; charset=\"UTF-8\"" ';
+
+        //Заголовки
+        foreach ($headers as $key => $value) {
+            if (!is_array($value)) {
+                $value = [$value];
+            }
+            foreach ($value as $valueItem) {
+                $valueItem = str_replace('"', '\"', $valueItem);
+                $shellCommand .= ' --header "'.$key.': '.$valueItem.'" ';
+            }
+        }
+        if ($certificateThumbprint) {
+            $shellCommand .= '--cert ' . $certificateThumbprint;
+        }
+        if ($requestBody) {
+            $shellCommand .= '--data \'' . $requestBody . '\'';
+        }
+
+        die($shellCommand);
+
+        /*$result = shell_exec($shellCommand);
+        if (!$result) {
+            throw new UCSoapTransportException('Не получен ответ на команду ' . $shellCommand);
+        }
+        $xml = html_entity_decode($result, ENT_QUOTES | ENT_HTML5);
+
+        //Ошибка SOAP
+        preg_match('/<faultstring.*>(.*)<\/faultstring>/sUu', $xml, $errorMatches);
+        if (isset($errorMatches[1]) && $errorMatches[1]) {
+            preg_match('/<mserror:source>(.*)<\/mserror:source>/s', $xml, $sourceMatches);
+            $source = null;
+            if (isset($sourceMatches[1]) && $sourceMatches[1]) {
+                $source = $sourceMatches[1];
+            }
+            throw new UCSoapCallException($action, $params, $source, $errorMatches[1]);
+        }
+
+
+        //XML результата
+        preg_match('/<' . $action . 'Result.*>(.*)<\/' . $action . 'Result>/sUu', $xml, $matches);
+        if (!isset($matches[1]) || !$matches[1]) {
+            //Запрос выполнен, пустой результат
+            preg_match('/<q1:' . $action . 'Response xmlns:q1=/s', $xml, $emptyResponseMatches);
+            if (isset($emptyResponseMatches[0]) && $emptyResponseMatches[0]) {
+                return true;
+            }
+
+            throw new UCSoapTransportException('Получен не SOAP-ответ на команду ' . $shellCommand . ': ' . $result);
+        }
+
+        $resultXML = $matches[1];
+
+        //die($resultXML);
+
+        return $resultXML;*/
+    }
 }
