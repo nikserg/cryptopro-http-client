a573d57ec4c062635c477d4f73c178e9826b8fdd
Nikita Zarubin <nikita-dimitrov@mail.ru>
2019-09-04 16:46:23 +0300

    Update CryptoproHttpClient.php

--- CryptoproHttpClient.php
+++ CryptoproHttpClient.php
@@ -1,8 +1,6 @@
 <?php
 namespace nikserg\cryptoprohttpclient;
 
-use yii\base\Exception;
-
 class CryptoproHttpClient {
 
     /**
@@ -36,43 +34,13 @@ class CryptoproHttpClient {
             $shellCommand .= ' --data \'' . $requestBody . '\'';
         }
 
-        //die($shellCommand);
-
         $result = shell_exec($shellCommand);
         if (!$result) {
             throw new \Exception('Не получен ответ на команду ' . $shellCommand);
         }
-        $xml = html_entity_decode($result, ENT_QUOTES | ENT_HTML5);
 
-        //Ошибка SOAP
-        /*preg_match('/<faultstring.*>(.*)<\/faultstring>/sUu', $xml, $errorMatches);
-        if (isset($errorMatches[1]) && $errorMatches[1]) {
-            preg_match('/<mserror:source>(.*)<\/mserror:source>/s', $xml, $sourceMatches);
-            $source = null;
-            if (isset($sourceMatches[1]) && $sourceMatches[1]) {
-                $source = $sourceMatches[1];
-            }
-            throw new \Exception($url. $errorMatches[1]);
-        }*/
-
+        $xml = $result;
 
         return $xml;
-        //XML результата
-        /*preg_match('/<' . $action . 'Result.*>(.*)<\/' . $action . 'Result>/sUu', $xml, $matches);
-        if (!isset($matches[1]) || !$matches[1]) {
-            //Запрос выполнен, пустой результат
-            preg_match('/<q1:' . $action . 'Response xmlns:q1=/s', $xml, $emptyResponseMatches);
-            if (isset($emptyResponseMatches[0]) && $emptyResponseMatches[0]) {
-                return true;
-            }
-
-            throw new UCSoapTransportException('Получен не SOAP-ответ на команду ' . $shellCommand . ': ' . $result);
-        }
-
-        $resultXML = $matches[1];
-
-        //die($resultXML);
-
-        return $resultXML;*/
     }
 }
