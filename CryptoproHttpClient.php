427e7ab80b2e05f83bec7ebf5a075d90f8aada51
Nikita Zarubin <nikita-dimitrov@mail.ru>
2019-09-04 10:00:27 +0300

    Update CryptoproHttpClient.php

--- CryptoproHttpClient.php
+++ CryptoproHttpClient.php
@@ -1,6 +1,8 @@
 <?php
 namespace nikserg\cryptoprohttpclient;
 
+use yii\base\Exception;
+
 class CryptoproHttpClient {
 
     /**
@@ -13,7 +15,7 @@ class CryptoproHttpClient {
     {
         $requestBody = str_replace("'", "'\"'\"'", $requestBody);
 
-        $shellCommand = self::$curlExec .
+        $shellCommand = 'yes "o" | '. self::$curlExec .
             ' -k -s '. $url . ' '.
             '--header "Content-Type: text/xml; charset=\"UTF-8\"" ';
 
@@ -24,38 +26,39 @@ class CryptoproHttpClient {
             }
             foreach ($value as $valueItem) {
                 $valueItem = str_replace('"', '\"', $valueItem);
-                $shellCommand .= ' --header "'.$key.': '.$valueItem.'" ';
+                $shellCommand .= ' --header "'.$key.': '.$valueItem.'"';
             }
         }
         if ($certificateThumbprint) {
-            $shellCommand .= '--cert ' . $certificateThumbprint;
+            $shellCommand .= ' --cert ' . $certificateThumbprint;
         }
         if ($requestBody) {
-            $shellCommand .= '--data \'' . $requestBody . '\'';
+            $shellCommand .= ' --data \'' . $requestBody . '\'';
         }
 
-        die($shellCommand);
+        //die($shellCommand);
 
-        /*$result = shell_exec($shellCommand);
+        $result = shell_exec($shellCommand);
         if (!$result) {
-            throw new UCSoapTransportException('Не получен ответ на команду ' . $shellCommand);
+            throw new \Exception('Не получен ответ на команду ' . $shellCommand);
         }
         $xml = html_entity_decode($result, ENT_QUOTES | ENT_HTML5);
 
         //Ошибка SOAP
-        preg_match('/<faultstring.*>(.*)<\/faultstring>/sUu', $xml, $errorMatches);
+        /*preg_match('/<faultstring.*>(.*)<\/faultstring>/sUu', $xml, $errorMatches);
         if (isset($errorMatches[1]) && $errorMatches[1]) {
             preg_match('/<mserror:source>(.*)<\/mserror:source>/s', $xml, $sourceMatches);
             $source = null;
             if (isset($sourceMatches[1]) && $sourceMatches[1]) {
                 $source = $sourceMatches[1];
             }
-            throw new UCSoapCallException($action, $params, $source, $errorMatches[1]);
-        }
+            throw new \Exception($url. $errorMatches[1]);
+        }*/
 
 
+        return $xml;
         //XML результата
-        preg_match('/<' . $action . 'Result.*>(.*)<\/' . $action . 'Result>/sUu', $xml, $matches);
+        /*preg_match('/<' . $action . 'Result.*>(.*)<\/' . $action . 'Result>/sUu', $xml, $matches);
         if (!isset($matches[1]) || !$matches[1]) {
             //Запрос выполнен, пустой результат
             preg_match('/<q1:' . $action . 'Response xmlns:q1=/s', $xml, $emptyResponseMatches);
